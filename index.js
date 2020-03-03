3856392bf5d65de4ee5248b867b794fb2d56b358
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-03 01:15:23 +0000

    car filteration working now

--- index.js
+++ index.js
@@ -64,20 +64,41 @@ app.get('/gender/g=:gender',(req,res)=>{
   }
 });
 
-// Accessing car owners data common between people & cars based on first_name
-app.get('apiData1/car_make=:first_name/apiData=:car_owns', (req, res)=>{
-  const nameParam = req.params.first_name;
-  const carParam = req.params.car_owns;
+// // Accessing car owners data common between people & cars based on first_name
+// app.get('apiData1/car_owner=:first_name/car_make=:car_owns', (req, res)=>{
+//   const nameParam = req.params.first_name;
+//   const carParam = req.params.car_owns;
+//
+//   let filteredArray = [];
+//   for(let i = 0; i < apiData1.length; i++) {
+//     if ((nameParam.toLowerCase() === apiData1[i].first_name.toLowerCase()) && (carParam.toLowerCase() === apiData1[i].car_make.toLowerCase())) {
+//       filteredArray.push(apiData1[i]);
+//     }
+//   }
+//   res.send(filteredArray);
+// });
 
-  let filteredArray = [];
-  for(let i = 0; i < apiData1.length; i++) {
-    if ((nameParam.toLowerCase() === apiData1[i].first_name.toLowerCase()) && (carParam.toLowerCase() === apiData1[i].car_make.toLowerCase())) {
-      filteredArray.push(apiData1[i]);
-    }
-  }
-  res.send(filteredArray);
+// Car Make and Car Modal Year Filter
+app.get('/q=:query1&q=:query2', (req,res) =>{
+    const first_nameParam = req.params.query1.toLowerCase();
+    const car_makeParam = req.params.query2.toLowerCase();
+        var filteredArray = [];
+        for (var i = 0; i < apiData1.length; i++) {
+            if ((first_nameParam === apiData1[i].first_name.toLowerCase()) && (car_makeParam == apiData1[i].car_make.toLowerCase())){
+                filteredArray.push(apiData1[i]);
+            }
+        }
+        if (filteredArray.toString() === '') {
+            res.send('Invalid parameter');
+        } else {
+            res.send(filteredArray);
+        }
 });
 
+
+// if ((userinput1 = jsondata1) && (userinput2 = jsondata2)) {
+//
+// }
 
 //Commenting out this part after setting route for public folder files.
 // app.get('/', (req, res)=> res.send('Hello World!'));
